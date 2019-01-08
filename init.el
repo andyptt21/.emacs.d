f6f67c1a377c5f300b318ceba209db1d4fe0089e
andyptt21 <andyptt21@gmail.com>
2019-01-07 21:47:02 -0500

    Updated comments on init.el

--- init.el
+++ init.el
@@ -1,6 +1,8 @@
 ;;; init.el --- Andy Patt's emacs configuration
-;; 
-;;
+;; This initialization file mostly provides support for writing literate R code with org-babel, Rmarkdown or Rnw in emacs
+;; I also use org-mode a lot, and occasionally upload to my wordpress blog
+;; The theme is a modified version of spacemacs light theme
+;; I am currently running Emacs 25.3.1 on macOS Mojave Version 10.14.1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; use-package
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -99,7 +101,7 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Personal; delete if you're not Andy
+;; Linking to my wordpress blog and OSC account
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; org2blog
 (use-package org2blog-autoloads
