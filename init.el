a2e7d348633001d7b95ec590141536777ce8c460
andyptt21 <andyptt21@gmail.com>
2019-01-11 11:53:06 -0500

    Update init.el

--- init.el
+++ init.el
@@ -109,20 +109,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Linking to my wordpress blog and OSC account
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; ;; org2blog
-;; (use-package org2blog
-;;   :config (setq org2blog/wp-blog-alist
-;;       '(("wordpress"
-;;          :url "https://thescientificshrimper.wordpress.com/xmlrpc.php"
-;;          :username "andyptt21"
-;;          :default-title "The Scientific Shrimper"
-;;          :default-categories ("org2blog" "emacs")
-;;          :tags-as-categories nil))))
-
-;; ;; Connect to Owens (OSC)
-;; (defun connect-remote ()
-;;   (interactive)
-;;   (dired "/ssh:osu8143@owens.osc.edu:/users/PAS1143/osu8143/"))
 (if (file-exists-p "~/.emacs.d/elisp/blog_and_osc_login.el") (load "~/.emacs.d/elisp/blog_and_osc_login.el") nil)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
